370c0fb6b2e91a2e57750780f416636fe7a8af55
EC2 Default User <ec2-user@ip-172-31-22-182.ap-northeast-1.compute.internal>
2022-06-11 04:46:10 +0000

    [start]編集機能

--- app/controllers/bookers_controller.rb
+++ app/controllers/bookers_controller.rb
@@ -23,6 +23,13 @@ class BookersController < ApplicationController
     @booker=Booker.find(params[:id])
   end
   
+  def update
+    booker=Booker.find(params[:id])
+    booker.update(booker_params)
+    redirect_to booker_path(booker.id)
+  end  
+    
+    
   private
   def booker_params
     params.require(:booker).permit(:title, :body)
